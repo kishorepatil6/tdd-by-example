0a41fc48097360f9bac4a06e4139af6333b45b0b
kishorepatil6 <kishorepatil.work@gmail.com>
2020-01-15 00:31:05 +0530

    save

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,10 +1,20 @@
 package guru.springframework;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    public static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount){
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object){
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount && this.getClass().equals(object.getClass());
     }
+
+    public abstract Money times(int multiplier);
 }
